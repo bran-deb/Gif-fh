78e71f840432161ed0841f69adc0300484974ce0
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2021-12-07 18:57:34 -0400

    The logic of the component was separated
    - creamos un directorio helpers donde pusimos la logica del componente
    - renderizamos el componente si cambia de categoria

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,30 +1,17 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+import { getGifs } from '../helpers/getgifs'
 import { GifGridItem } from './gifGridItem'
 
 export const GifGrid = ({ category }) => {
-    //hacemos que el componente se ejecute una sola vez
-    useEffect(() => {
-        getGifs()
-    }, [])
 
     const [images, setImages] = useState([])
 
-    const getGifs = async () => {
-        const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=i6WreDgXbyKNG06qdfOz08YlRDqCbtUx`
-        const resp = await fetch(url)
-        const { data } = await resp.json()
-
-        const gifs = data.map(img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url,
-            }
-        })
-        console.log(gifs)
-        setImages(gifs)
-    }
+    useEffect(() => {
+        getGifs(category)
+            .then(setImages)
+        // si category cambia volvemos a ejecutar este efecto
+    }, [category])
 
     return (
         <>
